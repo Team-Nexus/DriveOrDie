bb6c5281c6723851fec770d0ee55449412afc3a3
Teemu Karjalainen <t5kate01@students.oamk.fi>
2018-02-09 15:02:17 +0200

    MobileSupport
    Added support for mobile like touch controls:
    - 1 finger = accelerate
    - 2 fingers = backup
    - finger on the edge of the screen = turning to that direction
    
    Added menus and reset for mobile
    Changed some assets
    Added SIMPLE police AI

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameTimer : MonoBehaviour {
+
+    public Text textGameTimer;
+    public float minutes, seconds;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        minutes = (int)(Time.timeSinceLevelLoad / 60f);
+        seconds = (int)((Time.timeSinceLevelLoad - 5) % 60f);
+        textGameTimer.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+    }
+}
